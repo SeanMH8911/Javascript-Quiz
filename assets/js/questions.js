2a2adaad2caee1ac6d8b449fca7151092fa570ab
Sean Howe <seanmarchowe@gmail.com>
2023-01-23 14:14:07 +0000

    Initial Commit

---
+++ assets/js/questions.js
@@ -0,0 +1,100 @@
+let questions = [
+  {
+    title: "What is JavaScript?",
+    possibleAnswers: [
+      "JavaScript is a scripting language used to make the website interactive",
+      "JavaScript is an assembly language used to make the website interactive",
+      " JavaScript is a compiled language used to make the website interactive",
+      "None of the mentioned",
+    ],
+    correctAnswer:
+      "JavaScript is a scripting language used to make the website interactive",
+  },
+  {
+    title: "Which of the following is correct about JavaScript?",
+    possibleAnswers: [
+      "JavaScript is an Object-Based language",
+      " JavaScript is Assembly-language",
+      "JavaScript is an Object-Oriented language",
+      " JavaScript is a High-level language",
+    ],
+    correctAnswer: "JavaScript is an Object-Based language",
+  },
+  {
+    title:
+      "Among the given statements, which statement defines closures in JavaScript?",
+    possibleAnswers: [
+      " JavaScript is a function that is enclosed with references to its inner function scope",
+      "JavaScript is a function that is enclosed with references to its lexical environment",
+      "JavaScript is a function that is enclosed with the object to its inner function scope",
+      "None of the mentioned",
+    ],
+    correctAnswer:
+      "JavaScript is a function that is enclosed with references to its lexical environment",
+  },
+  {
+    title:
+      "Arrays in JavaScript are defined by which of the following statements?",
+    possibleAnswers: [
+      "It is an ordered list of values",
+      "It is an ordered list of objects",
+      "It is an ordered list of string",
+      "It is an ordered list of functions",
+    ],
+    correctAnswer: "It is an ordered list of values",
+  },
+  {
+    title:
+      "Will the following JavaScript code work? var js = (function(x) {return x*x;}(10));",
+    possibleAnswers: [
+      "Exception will be thrown",
+      "Memory leak",
+      "Error",
+      "Yes, perfectly",
+    ],
+    correctAnswer: "Yes, perfectly",
+  },
+  {
+    title: "Which of the following is not javascript data types?",
+    possibleAnswers: [
+      "Null type",
+      "Undefined type",
+      "Number type",
+      "All of the mentioned",
+    ],
+    correctAnswer: "All of the mentioned",
+  },
+  {
+    title:
+      "Where is Client-side JavaScript code is embedded within HTML documents?",
+    possibleAnswers: [
+      "A URL that uses the special javascript:code",
+      "A URL that uses the special javascript:protocol",
+      "A URL that uses the special javascript:encoding",
+      "A URL that uses the special javascript:stack",
+    ],
+    correctAnswer: "A URL that uses the special javascript:protocol",
+  },
+  {
+    title:
+      "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    possibleAnswers: ["Position", "Window", "Standard", " Location"],
+    correctAnswer: "Window",
+  },
+  {
+    title:
+      "Which of the following can be used to call a JavaScript Code Snippet?",
+    possibleAnswers: [
+      "Function/Method",
+      "Preprocessor",
+      "Triggering Event",
+      "RMI",
+    ],
+    correctAnswer: "Function/Method",
+  },
+  {
+    title: "Which of the following scoping type does JavaScript use?",
+    possibleAnswers: ["Sequential", "Segmental", "Lexical", " Literal"],
+    correctAnswer: "Lexical",
+  },
+];
